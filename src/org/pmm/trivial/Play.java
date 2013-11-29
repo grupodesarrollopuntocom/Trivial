14e306748e0d867ee8e8513c91c9bcf533cd1953
Campus <Campus@Aula1-pc6>
2013-11-29 12:01:08 +0100

    hilo

--- src/org/pmm/trivial/Play.java
+++ src/org/pmm/trivial/Play.java
@@ -1,16 +1,56 @@
 package org.pmm.trivial;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ProgressBar;
+
 
 public class Play extends Activity {
-
+	
+	ProgressBar barraTiempo;
+	private HiloProgreso hilo;
+	int progreso;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_play);
+		
+		barraTiempo=(ProgressBar)findViewById(R.id.miProgressbar);
+		hilo = new HiloProgreso();
+		hilo.execute();
 	}
+	
+	public class HiloProgreso extends AsyncTask<Void, Void, Void>{
+		protected void onPostExecute(Void result){
+			
+		}
+		@Override
+		protected void onPreExecute() {
+			barraTiempo.setMax(1000);
+		}
+		@Override
+		protected void onProgressUpdate(Void... values) {
+			barraTiempo.incrementProgressBy(100);
+		}
+		
+		@Override
+		protected Void doInBackground(Void... arg0) {
+			while(!isCancelled()){
+				try{
+					Thread.sleep(1000);
+					publishProgress();
+				}catch(InterruptedException e){
+					
+				}					
+			}
+			return null;
+		}
+		
+}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
