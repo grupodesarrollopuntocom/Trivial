6b3ca24503f81b88c71cf5760a4290362638f51c
gabrielfcr <DAM@gabriel-PC>
2013-11-29 11:22:43 +0100

    Carga de atributos

--- src/org/pmm/trivial/Play.java
+++ src/org/pmm/trivial/Play.java
@@ -3,13 +3,27 @@ package org.pmm.trivial;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Play extends Activity {
+	private TextView tv1, tv2;
+	private Button bt1, bt2, bt3, bt4;
+	private ProgressBar pgb;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_play);
+		tv1 = (TextView)findViewById(R.id.miTextoPregunta);
+		tv2 = (TextView)findViewById(R.id.miTextoScore);
+		bt1 = (Button)findViewById(R.id.miBoton1);
+		bt2 = (Button)findViewById(R.id.miBoton2);
+		bt3 = (Button)findViewById(R.id.miBoton3);
+		bt4 = (Button)findViewById(R.id.miBoton4);
+		pgb = (ProgressBar)findViewById(R.id.miProgressbar);
 	}
 
 	@Override
